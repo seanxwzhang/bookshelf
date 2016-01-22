d34c913f296fc5ef7487c8e1215586263b40b903
XiaowenZhang <xiaowenzhang@0587365894.wireless.umich.net>
2016-01-22 17:20:59 -0500

    second commit

---
+++ js/behavior.js
@@ -0,0 +1,43 @@
+//Add interactivity so the user could manage their bookshelf
+
+var bookInputForm = document.getElementById("add_book");
+var addButton = document.getElementById("add_button");
+var bookList = document.getElementById("booklist");
+var searchForm = document.getElementById("search_form");
+
+//Add a new book
+var addBook = function() {
+  console.log("Adding book");
+  // When the button is pressed
+  // Create a new list item with the text from bookInputForm
+  
+}
+//Edit an existing book
+var editBook = function() {
+  console.log("Editing book");
+  // When the button is pressed
+
+}
+//Delete an existing book
+var deleteBook = function() {
+  console.log("Deleting book");
+}
+//Search books based on name
+var searchName = function() {
+  console.log("Searching book");
+}
+
+//Search books based on category
+var searchCategory = function() {
+  console.log("Searching category");
+}
+
+//Clear search result
+var clearSearch = function() {
+  console.log("Clearing search result");
+}
+
+//Exit edit mode
+var exitEdit = function() {
+  console.log("Exiting edit mode");
+}
